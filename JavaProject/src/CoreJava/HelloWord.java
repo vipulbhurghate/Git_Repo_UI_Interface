bcdd18ca2d4206cf7fe192a1ff662f49e66b3e56
vipulbhurghate <bhurghate.vipul@gmail.com>
2018-09-04 16:31:26 +0530

    Signed-off-by: vipulbhurghate <bhurghate.vipul@gmail.com>

--- JavaProject/src/CoreJava/HelloWord.java
+++ JavaProject/src/CoreJava/HelloWord.java
@@ -4,7 +4,9 @@ public class HelloWord {
 
 	public static void main(String[] args) 
 	{
-	System.out.println("Hello Java");
+	
+		System.out.println("Hello Java");
+		System.out.println("How are you");
 
 	}
 
